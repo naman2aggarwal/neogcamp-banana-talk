759be4c75869b133a2f8e7195325c308a48ea935
Naman Aggarwal <namanaggarwal125@gmail.com>
2020-12-17 23:22:58 +0530

    fix: fixed cors and encoded uri

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ let bananaTextArea = document.querySelector("#bananaTextArea");
 
 let translateButton = document.querySelector("#btn-translate");
 
+let url =
+  "https://cors-anywhere.herokuapp.com/https://api.funtranslations.com/translate/minion.json";
+
 translateButton.addEventListener("click", translateToBanana);
 
 function translateToBanana() {
   let inputTxt = inputTextArea.value;
-  let url = `https://api.funtranslations.com/translate/minion.json?text=${inputTxt}`;
-  fetch(url)
+
+  fetch(generateUrl(inputTxt))
     .then((response) => response.json())
     .then((data) => {
       bananaTextArea.value = data.contents.translated;
     })
     .catch(() => alert("Some error occured!, Please try after sometime."));
+}
+
+function generateUrl(val) {
+  let encodedValue = encodeURI(val);
+  return `${url}?text=${encodedValue}`;
 }
