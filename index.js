036587ac5ea59428dc0e9b28c6ecaac55be1f427
Naman Aggarwal <namanaggarwal125@gmail.com>
2020-12-17 20:02:09 +0530

    feat: added minion translation

---
+++ index.js
@@ -0,0 +1,17 @@
+let inputTextArea = document.querySelector("#inputTextArea");
+let bananaTextArea = document.querySelector("#bananaTextArea");
+
+let translateButton = document.querySelector("#btn-translate");
+
+translateButton.addEventListener("click", translateToBanana);
+
+function translateToBanana() {
+  let inputTxt = inputTextArea.value;
+  let url = `https://api.funtranslations.com/translate/minion.json?text=${inputTxt}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      bananaTextArea.value = data.contents.translated;
+    })
+    .catch(alert("Some error occured!, Please try after sometime."));
+}
